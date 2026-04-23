391acdd2ae637fffb2ca1b42ae1ac3aee4665e30
LuisSalazarDIIM <Luiss_salazar07@hotmail.com>
2026-04-23 10:46:28 -0600

    Initial project setup
    Creation of Abitat.Business
    Creation of Abitat.Data
    Creation of Abitat.Entities
    
    Web.config configuration
    
    Creation of Connection.vb in Abitat.Data
    
    Added reference to System.Configuration in Abitat.Data

---
+++ Abitat.Data/Connection.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Configuration
+Imports System.Data.SqlClient
+
+Public Class Connection
+
+    Private Const CONNECTION_NAME As String = "AbitatDB"
+    Public Shared Function GetConnection() As SqlConnection
+        Dim cs As ConnectionStringSettings =
+            ConfigurationManager.ConnectionStrings(CONNECTION_NAME)
+
+        If cs Is Nothing OrElse String.IsNullOrWhiteSpace(cs.ConnectionString) Then
+            Throw New ConfigurationErrorsException(
+                $"The connection string was not found '{CONNECTION_NAME}' in Web.config.")
+        End If
+
+        Return New SqlConnection(cs.ConnectionString)
+    End Function
+
+End Class
